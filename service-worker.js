f784f0f551a07aec742966951c8cb79b0ece7f6b
caxspro <167343803+caxspro@users.noreply.github.com>
2026-06-11 16:52:58 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,27 @@
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(clients.claim());
 });
 
-// Обработчик push-уведомлений (можно будет использовать позже)
-self.addEventListener("push", (event) => {
+// Обрабатываем push-событие
+self.addEventListener("push", event => {
   if (event.data) {
     const data = event.data.json();
     const options = {
-      body: data.text,
-      icon: "https://cdn-icons-png.flaticon.com/512/134/134718.png",
-      badge: "https://cdn-icons-png.flaticon.com/512/134/134718.png",
-      vibrate: [200, 100, 200]
+      body: data.body,
+      icon: "/supa-chat/icon.png",
+      badge: "/supa-chat/icon.png",
+      vibrate: [200, 100, 200],
     };
-    event.waitUntil(self.registration.showNotification(data.sender, options));
+    event.waitUntil(self.registration.showNotification(data.title, options));
   }
 });
 
-self.addEventListener("notificationclick", (event) => {
+// При клике на уведомление открываем чат
+self.addEventListener("notificationclick", event => {
   event.notification.close();
   event.waitUntil(clients.openWindow("/supa-chat/supa-chat.html"));
 });
